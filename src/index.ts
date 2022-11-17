f8a091eb6e3be58ac0e326015840210216cdf71a
gillsonpassos <oliveirapassos07@outlook.com>
2022-11-17 11:43:10 -0300

    Vantagens e Desvantagens e DRY-YAGNI-KISS-SOC-MVP

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-import funcao from './mod';
+/*
+Vantagens e Desvantagens do S.O.L.I.D.
 
-funcao();
+Vantagens:
+- Codigo modular
+- Codigo reutilizavel (D.R.Y - Don't repeat yourself)
+- Codigo testavel, baixo acoplamento
+- Baixo acoplamento e alta coesão
+- Codigo expansive
+- Separations of concepts (Separação de conceitos)
+- Facil manutenção
+  #########################=########################
+Desvantagens:
+- Complexidade
+- Quantidade de codigo digitado aumenta
+- Tempo de desenvolvimento aumenta bastante
+- Tenha cuidado com: YAGNI,KISS (you aren't gonna need it, Keep it simple; stupid!)
+*/
