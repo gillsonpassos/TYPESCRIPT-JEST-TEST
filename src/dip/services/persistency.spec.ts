9be2dcf015b50e5b2a174578862dca79c99e0b3f
gillsonpassos <oliveirapassos07@outlook.com>
2022-11-17 15:38:32 -0300

    introdução ao JEST

---
+++ src/dip/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('TESTANDO ALGUMA COISA', () => {
+  it('should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('TESTANDO OUTRA COISA', () => {
+  test('should return Molusco', () => {
+    const name = 'Molusco';
+    expect(name).toBe('Molusco');
+  });
+});
