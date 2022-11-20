2612f4f60c7497db41ef1fcd4dac793986f9ea52
gillsonpassos <oliveirapassos07@outlook.com>
2022-11-20 12:12:46 -0300

    Testando discount

---
+++ src/dip/classes/discount.spec.ts
@@ -0,0 +1,29 @@
+import {
+  Discount,
+  FiftyPercentDiscount,
+  NoDiscount,
+  TenPercentDiscount,
+} from './discount';
+
+const creatSut = (className: new () => Discount): Discount => {
+  return new className();
+};
+
+describe('Discount', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should have no discount', () => {
+    const sut = creatSut(NoDiscount);
+    expect(sut.calculate(10.99)).toBeCloseTo(10.99);
+  });
+
+  it('should applay 50% discount on price', () => {
+    const sut = creatSut(FiftyPercentDiscount);
+    expect(sut.calculate(150.5)).toBeCloseTo(75.25);
+  });
+
+  it('should applay 10% discount on price', () => {
+    const sut = creatSut(TenPercentDiscount);
+    expect(sut.calculate(10)).toBeCloseTo(9);
+  });
+});
--- src/dip/classes/product.spec.ts
+++ src/dip/classes/product.spec.ts
@@ -7,7 +7,7 @@ const creatSut = (name: string, price: number): Product => {
 describe('Product', () => {
   afterEach(() => jest.clearAllMocks());
 
-  it('should return undefined', () => {
+  it('should have properties name and price', () => {
     const sut = creatSut('Camiseta', 49.9);
     expect(sut).toHaveProperty('name', 'Camiseta');
     expect(sut.price).toBeCloseTo(49.9);
